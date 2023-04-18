c7123c76eba5cd6b355c6b94df5a82f595f7a308
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-04-17 22:47:39 -0300

    Implementing is_admin column in users table

--- database/migrations/2023_03_24_171345_create_users_table.php
+++ database/migrations/2023_03_24_171345_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('cpf')->unique();
+            $table->boolean('is_admin')->default(0);
         });
     }
 
