573041107b2f9ba8ab5808ddaaa56ef1e454153d
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-04-18 16:17:08 -0300

    Modifying routes

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,36 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(('guest'))->group(function() {
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::middleware('guest')->group(function() {
     Route::get('/register', [AuthController::class, 'insertView'])->name('register'); //
     Route::post('register', [AuthController::class, 'insertAction'])->name('register_action'); //
     Route::get('/login', [AuthController::class, 'loginView'])->name('login'); //
     Route::post('/login', [AuthController::class, 'loginAction'])->name('login_action'); //
 });
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware('admin')->group(function() {
+     // cabelereiras
+    Route::post('/hairdresser', [HairdresserController::class, 'insert']);
+    Route::put('/hairdresser/edit/{id}', [HairdresserController::class, 'update']);
+    Route::delete('/hairdresser/delete/{id}', [HairdresserController::class, 'delete']);
+    Route::get('/hairdresser/{id}', [HairdresserController::class, 'getInfo']);
+    Route::get('/hairdressers', [HairdresserController::class, 'getAll']);
+
+    // hd_services
+    // Route::get('/services', [ServiceController::class, 'getAll']);
+    // Route::get('/service/{id}', [ServiceController::class, 'getOne']);
+    // Route::post('/service', [ServiceController::class, 'insert']);
+    // Route::put('/service/edit/{id}', [ServiceController::class, 'update']);
+    // Route::delete('/service/delete/{id}', [ServiceController::class, 'delete']);
+
+    // hd_done_services
+    // Route::get('/done_services', [DoneServiceController::class, 'getDoneServices']); 
+    // Route::get('/done_service/{id}', [DoneServiceController::class, 'getOne']);
+    // Route::post('/done_service', [DoneServiceController::class, 'insert']);
+    // Route::delete('/done_service/delete/{id}', [DoneServiceController::class, 'delete']);
+});
 
 Route::middleware('auth')->group(function(){
     Route::get('/auth/logout', [AuthController::class, 'logout'])->name('logout'); //
@@ -27,13 +49,6 @@ Route::middleware('auth')->group(function(){
     Route::put('/user/edit/{id}', [UserController::class, 'updateAction'])->name('edit_user_action'); //
     Route::delete('/user/delete/{id}', [UserController::class, 'delete'])->name('delete_user_action'); //
 
-    // cabelereiras
-    Route::post('/hairdresser', [HairdresserController::class, 'insert']);
-    Route::put('/hairdresser/edit/{id}', [HairdresserController::class, 'update']);
-    Route::delete('/hairdresser/delete/{id}', [HairdresserController::class, 'delete']);
-    Route::get('/hairdresser/{id}', [HairdresserController::class, 'getInfo']);
-    Route::get('/hairdressers', [HairdresserController::class, 'getAll']);
-
     // appointments
     Route::get('/appointments', [AppointmentController::class, 'getMyAppointments'])->name('user_appointments'); //
     Route::get('/appointment', [AppointmentController::class, 'setAppointmentView'])->name('set_appointment'); //
@@ -42,20 +57,7 @@ Route::middleware('auth')->group(function(){
     Route::put('/appointment/edit/{id}', [AppointmentController::class, 'updateAction'])->name('edit_appointment_action'); //
     Route::delete('/appointment/delete/{id}', [AppointmentController::class, 'delete'])->name('delete_appointment_action'); //
     
-    // // hd_services
-    // Route::get('/services', [ServiceController::class, 'getAll']);
-    // Route::get('/service/{id}', [ServiceController::class, 'getOne']);
-    // Route::post('/service', [ServiceController::class, 'insert']);
-    // Route::put('/service/edit/{id}', [ServiceController::class, 'update']);
-    // Route::delete('/service/delete/{id}', [ServiceController::class, 'delete']);
-
-    // // hd_done_services
-    // Route::get('/done_services', [DoneServiceController::class, 'getDoneServices']); 
-    // Route::get('/done_service/{id}', [DoneServiceController::class, 'getOne']);
-    // Route::post('/done_service', [DoneServiceController::class, 'insert']);
-    // Route::delete('/done_service/delete/{id}', [DoneServiceController::class, 'delete']);
-
-    // // hd_evaluation
+    //  hd_evaluation
     // Route::get('/evaluations', [EvaluationController::class, 'getMyEvaluations']);
     // Route::get('/evaluation/{id}', [EvaluationController::class, 'getOne']);
     // Route::get('/evaluations/{id}', [EvaluationController::class, 'getHairdresserEvaluations']);
