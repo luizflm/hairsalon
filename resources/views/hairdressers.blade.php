2567c73dc5008b126c72ece883ef491393049c21
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-04-20 18:56:43 -0300

    Edit in hairdressers view

--- resources/views/hairdressers.blade.php
+++ resources/views/hairdressers.blade.php
@@ -19,30 +19,32 @@ increaseBanner="true"
                         @php
                             $row = 0;
                         @endphp
-                        @foreach($hairdressers as $hairdresser)
-                            @php
-                                $row++;
-                            @endphp
-                            <tr>
-                                <th scope="row">{{$row}}</th>
-                                <td>{{$hairdresser['name']}}</td>
-                                <td>{{$hairdresser['specialties']}}</td>  
-                                <td>
-                                    <div class="d-flex align-items-center justify-content-center">
-                                        <a href={{route('edit_hairdresser', $hairdresser['id'])}} class="btn btn-edit me-4">Editar</a>
-                                        <form method="POST" action={{route('delete_hairdresser_action', $hairdresser['id'])}}>
-                                            @csrf
-                                            @method('DELETE')
-                                            
-                                            <button type="submit" class="btn btn-delete my-2" 
-                                            onclick="return confirm('Deseja mesmo deletar o cabelereiro(a)?');">
-                                                Deletar
-                                            </button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
+                        @if(!empty($hairdressers))
+                            @foreach($hairdressers as $hairdresser)
+                                @php
+                                    $row++;
+                                @endphp
+                                <tr>
+                                    <th scope="row">{{$row}}</th>
+                                    <td>{{$hairdresser['name']}}</td>
+                                    <td>{{$hairdresser['specialties']}}</td>  
+                                    <td>
+                                        <div class="d-flex align-items-center justify-content-center">
+                                            <a href={{route('edit_hairdresser', $hairdresser['id'])}} class="btn btn-edit me-4">Editar</a>
+                                            <form method="POST" action={{route('delete_hairdresser_action', $hairdresser['id'])}}>
+                                                @csrf
+                                                @method('DELETE')
+                                                
+                                                <button type="submit" class="btn btn-delete my-2" 
+                                                onclick="return confirm('Deseja mesmo deletar o cabelereiro(a)?');">
+                                                    Deletar
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
