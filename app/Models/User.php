14ab0a7badf597a28b965f2577083de5953cf253
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-05-01 22:26:26 -0300

    Removing useless methods in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,8 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use HasFactory;
     
@@ -32,13 +30,5 @@ class User extends Authenticatable implements JWTSubject
 
     public function appointments() {
         return $this->hasMany(Appointment::class);
-    }
-
-    public function getJWTIdentifier() {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims() {
-        return [];
     }
 }
