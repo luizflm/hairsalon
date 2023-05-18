69821074e11c9b51a3bcd669eb12e57db98fa5dc
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-05-18 19:13:56 -0300

    Updating the way of declare all of the controllers in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AppointmentController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AvailabilityController;
-use App\Http\Controllers\DoneServiceController;
-use App\Http\Controllers\HairdresserController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\{
+    HomeController,
+    AuthController,
+    AdminController,
+    HairdresserController,
+    ServiceController,
+    AppointmentController,
+    DoneServiceController,
+    AvailabilityController,
+    UserController,
+};
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home'); 
