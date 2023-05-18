bd86ad08f0d9f80fd964e64e2a3b31208ed38671
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-05-18 18:25:53 -0300

    Removing comments from HomeController's index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,19 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index() {    
-        // pegando usuário logado
         $user = Auth::user();
 
-        // pegando os todos os hairdressers
         $hairdressers = Hairdresser::all();
         foreach($hairdressers as $hairdresser) { 
-            // transformando a string de especialidades em um array
             $specialties = explode(', ', $hairdresser['specialties']);
-            // substituindo a string pelo array
             $hairdresser['specialties'] = $specialties;
 
-            // trocando o nome da imagem pelo link até a storage (onde está a imagem)
             $hairdresser['avatar'] = asset('/storage/'.$hairdresser['avatar']);
         }
 
-        return view('pages.home', ['user' => $user, 'hairdressers' => $hairdressers,]);
+        return view('pages.home', [
+            'user' => $user,
+            'hairdressers' => $hairdressers,
+        ]);
     }
 }
