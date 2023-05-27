78a57882ea1ed9021a0144a616eabb8fa3a14b95
Luiz Felipe de Lima <lepinholima@gmail.com>
2023-05-27 17:27:28 -0300

    Working with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Appointment;
+use App\Models\Hairdresser;
+use App\Models\HairdresserAvailability;
+use App\Models\HairdresserService;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +19,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        Hairdresser::factory()
+        ->count(5)
+        ->has(HairdresserService::factory()->count(3), 'services')
+        ->has(HairdresserAvailability::factory()->count(5), 'availability')
+        ->has(Appointment::factory()->count(10))
+        ->create();
     }
 }
